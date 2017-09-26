27cb01dd230179d3e4b76f83c6bbf310e67b56e8
carlosEAM <carlos.alford@gmail.com>
2017-09-26 11:41:30 +0100

    Set HTML deck of cards

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/*
+ * TODO:
+ * initialiser function
+ * shuffle cards
+ * flip card on click
+ * check if cards match
+ * check if game is finished
+ * congratulations popup
+ * restart button
+ * timer
+ * star rating
+ * moves counter
+ */
