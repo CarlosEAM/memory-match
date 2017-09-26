549cdc2179d475f39a9b5f11a2b7a054697e3816
carlosEAM <carlos.alford@gmail.com>
2017-09-26 14:54:35 +0100

    Flip card on click
    The commit deals with the card flip when a user clicks.
    It also introduces a cetral gameSettings object which will
    help with values needed outside the function scopes.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 /*
  * TODO:
- * flip card on click
  * check if cards match
  * check if game is finished
  * congratulations popup
@@ -10,25 +9,51 @@
  * moves counter
  */
 
+let gameSettings = {};
+
 /**
 * @description initialise the game
 */
-let initGame = function(){
-  let cardDeck = $('.cards');
+let initGame = function() {
+  // init settings
+  gameSettings.GAME_BOARD = $('.game-board');
+  gameSettings.cardsDeck = $('.cards');
+  gameSettings.cardsDeckSize = gameSettings.cardsDeck.length;
+  gameSettings.checkCardFlip = false;
 
   // shuffle cards
-  shuffleCards(cardDeck);
+  shuffleCards(gameSettings.cardsDeck);
+
+  // set event listener on game board, catch it blubbling and call playerMove();
+  gameSettings.GAME_BOARD.on('click', 'div', playerMove);
 
 };
 
+/**
+* @description Check players progress on card flips
+* @param {$(this)} $(this) - the element caught on.click
+*/
+let playerMove = function() {
+  // flip the card
+  flipCard($(this));
+
+};
+
+/**
+* @description Flip over card to either hide or show it
+* @param {Object} card - the div element the user clicked
+*/
+let flipCard = function(card) {
+  	card.toggleClass('flip-card');
+};
 
 /**
 * @description Shuffles the deck of cards regarless of size
-* @param {Object} cards - a collection of <div> elements
+* @param {Array} cards - a collection of <div> elements
 */
-let shuffleCards = function(cards){
+let shuffleCards = function(cards) {
   let shuffle = 0;
-  for (let deckSize=cards.length; deckSize > 0; deckSize--){
+  for (let deckSize=cards.length; deckSize > 0; deckSize--) {
   	shuffle = Math.round(Math.random() * (deckSize - 1));
   	$(cards[shuffle]).css('order',deckSize);
   	cards.splice(shuffle,1);
