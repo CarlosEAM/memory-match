af3841a21db7c248323d57ceb624caa31c5d0461
carlosEAM <carlos.alford@gmail.com>
2017-09-26 11:45:07 +0100

    Create cards shuffle function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 /*
  * TODO:
- * initialiser function
- * shuffle cards
  * flip card on click
  * check if cards match
  * check if game is finished
@@ -11,3 +9,31 @@
  * star rating
  * moves counter
  */
+
+/**
+* @description initialise the game
+*/
+let initGame = function(){
+  let cardDeck = $('.cards');
+
+  // shuffle cards
+  shuffleCards(cardDeck);
+
+};
+
+
+/**
+* @description Shuffles the deck of cards regarless of size
+* @param {Object} cards - a collection of <div> elements
+*/
+let shuffleCards = function(cards){
+  let shuffle = 0;
+  for (let deckSize=cards.length; deckSize > 0; deckSize--){
+  	shuffle = Math.round(Math.random() * (deckSize - 1));
+  	$(cards[shuffle]).css('order',deckSize);
+  	cards.splice(shuffle,1);
+  }
+};
+
+// prepare game after page load
+$(initGame);
