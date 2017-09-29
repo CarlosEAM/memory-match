7b7b65c8a36e0e6f58b07453c9baccec53856250
carlosEAM <carlos.alford@gmail.com>
2017-09-29 14:56:48 +0100

    Set end of game detection function with modal
    This commit focused mainly on the logic to check all cards
    have been paired up which triggers the bootstrap modal.
    
    However during the implementation there were a few performance
    issues found which were addressed at the same time. The functions
    which were improved:
      - initGame()
      - checkCardsMatch()

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 /*
  * TODO:
- * check if game is finished
- * congratulations popup
  * restart button
  * timer
  * star rating
@@ -15,19 +13,22 @@ let gameSettings = {};
 */
 let initGame = function() {
   // init settings
-  gameSettings.GAME_BOARD = $('.game-board');
-  gameSettings.cardsDeck = $('.cards');
-  gameSettings.cardsDeckSize = gameSettings.cardsDeck.length;
+  let cardsDeck = $('.cards');
+  gameSettings.cardsToReset = $('.cards');
+  gameSettings.cardsDeckSize = cardsDeck.length;
   gameSettings.previousCard = null;
   gameSettings.cardsFlipped = 0;
   gameSettings.MAX_CARDS_PAIRS = gameSettings.cardsDeckSize / 2;
   gameSettings.cardPairsFound = 0;
 
   // shuffle cards
-  shuffleCards(gameSettings.cardsDeck);
+  shuffleCards($('.cards'));
 
-  // set event listener on game board, catch it blubbling and call playerMove();
-  $('.cards').on('click', playerMove);
+  // set event listeners
+  cardsDeck.on('click', playerMove);
+
+  // set event listener on reset buttons
+  $('.reset').on('click', resetGame);
 
 };
 
@@ -47,6 +48,14 @@ let playerMove = function() {
       checkCardsMatch($(this));
       gameSettings.cardsFlipped = 0;
     }
+  }
+
+  // when all pairs found activate popup window
+  if (gameSettings.cardPairsFound == gameSettings.MAX_CARDS_PAIRS) {
+    setTimeout(function(){
+      // wait for the last cards to confirm pairing before modal MESSAGE
+      $('#myModal').modal();
+    }, 800);
   }
 
 };
@@ -79,7 +88,7 @@ let shuffleCards = function(cards) {
 let checkCardsMatch = function(card){
   // to keep previous card value in this context
   let previousCard = gameSettings.previousCard;
-  if (card.attr('data-card') == previousCard.attr('data-card')){
+  if (card.data('card') == previousCard.data('card')){
     // if pair is found
     previousCard.off();
     card.off();
@@ -90,7 +99,6 @@ let checkCardsMatch = function(card){
       card.toggleClass('rubberBand animated');
     }, 200, card, previousCard);
     setTimeout(function(){
-      console.log("you win")
       previousCard.toggleClass('rubberBand animated');
       card.toggleClass('rubberBand animated');
     }, 1000, card, previousCard);
@@ -109,6 +117,21 @@ let checkCardsMatch = function(card){
       card.toggleClass('wobble animated');
   	}, 1000, card, previousCard);
   }
+}
+
+let resetGame = function(){
+  // flip all cards back.
+  setTimeout(function(){
+    $('.flip-card').on('click', playerMove);
+    flipCard($('.flip-card'));
+  }, 400);
+  // reset all settings and shuffle cards after they are turned
+  setTimeout(function(){
+    gameSettings.previousCard = null;
+    gameSettings.cardsFlipped = 0;
+    gameSettings.cardPairsFound = 0;
+    shuffleCards($('.cards'));
+  }, 800);
 }
 
 // prepare game after page load
